aa069f8caf6264cbfdc39470e402ea16bee9b00f
Yengzzkie <gatchalian.manuel@gmail.com>
2025-06-18 22:13:55 -0400

    temporarily disabled /register page

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,16 +1,16 @@
 "use client";
 import RegistrationForm from "../components/RegistrationForm";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+// import { useSession } from "next-auth/react";
+// import { useRouter } from "next/navigation";
 
 const page = () => {
-  const { data: session } = useSession();
-  const router = useRouter();
+  // const { data: session } = useSession();
+  // const router = useRouter();
 
-  if (session) {
-    router.push("/");
-    return null;
-  }
+  // if (session) {
+  //   router.push("/");
+  //   return null;
+  // }
 
   return (
     <div>
